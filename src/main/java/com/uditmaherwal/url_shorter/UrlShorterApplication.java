68917c909631363dfe0de63432000f478512996c
Udit Maherwal <51200625+uditmaherwal@users.noreply.github.com>
2026-06-29 11:02:24 +0530

    Initialized springboot project : url-shortner

---
+++ src/main/java/com/uditmaherwal/url_shorter/UrlShorterApplication.java
@@ -0,0 +1,13 @@
+package com.uditmaherwal.url_shorter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShorterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlShorterApplication.class, args);
+	}
+
+}
